6f84c830fe7d571b86baea02a5e077a9f8d148f9
merongmerongmerona-ovg <cw2510907@sonline20.sen.go.kr>
2026-08-13 19:30:42 +0900

    Create users.ts

---
+++ src/constants/users.ts
@@ -0,0 +1,6 @@
+export const ALLOWED_USERS = [
+  { id: "merong_777", password: "123", nickname: "메롱메롱메로나", avatar: "👑" },
+  { id: "test2", password: "123", nickname: "부메롱", avatar: "💎" },
+  { id: "frog", password: "123", nickname: "개굴", avatar: "🐸" },
+  { id: "yunjjang", password: "123", nickname: "윤성짱", avatar: "🦄" },
+];
